4c1d72a1dc96305ca0fdbfe3f6b640b663765f16
luchk <30504944+luchk@users.noreply.github.com>
2017-11-30 22:25:54 +0200

    модифікація лабораторної роботи. видалив непотрібне, створив власні функції, зробив код красивішим

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <math.h>
 #include <iostream>
 #include <iomanip>
+#include "fourierSeries.h"
 
 using namespace std;
 
@@ -11,14 +12,18 @@ int main()
     int N=2000; // кількість поділок еа які поділений мій графік
     int t1=0; // рахує кількість ітераційциклу табулювання
     int numbers = 2010; // розмір масиву для збереження данних
-    double w, ao, bk, ak, sm, aoo = 0, akk = 0, bkk = 0, smt, k, tt = 0.0, smtt = 0.0;
+    double bk, ak, akk = 0, bkk = 0, smt, k, tt = 0.0, smtt = 0.0;
+    double aoo = 0; // коефіциіент а0 (а нульове)
     double T = 1e-3; // період
     double h = T/N; // дорівнює період поділити на 2000
-    double t = 0, s;
+    double t = 0; // наша вісь Х по якій ми і табулюємо функцію
+    double s; // наша вісь У якій ми присвоюємо значення якого набуває наша функція у точці t
     double h2 = 2*h/T; // частинка формули
     double PI = 3.1415926535897932384626433832795; // число пі
+    double w = (2*PI)/T; // частинка формул
     double a[numbers]; // зберігаються всі значення змінної s (по осі У)
     double b[numbers]; // зберігаються всі значення змінної t (по осі Х)
+
 
     FILE*ffur; // оголошуємо змінні файлу
     FILE*ffurr;
@@ -49,10 +54,10 @@ int main()
             b[t1] = t;
 //            cout << t << " " << s << endl;
         }
-        fprintf(ffur,"%.10f\t", t);
+        fprintf(ffur,"%.10f\t", t); // записуємо у файл отримані значення
         fprintf(ffur,"%.10f\n", s);
 
-        t+=0.001/N;
+        t += 0.001/N; // додаємо значень до нашого лічильника
         t1++;
     }
 
@@ -60,45 +65,38 @@ int main()
     fclose(ffur);
     for (int i = 0; i < numbers; i++)
     {
-        ao = a[i];
-        aoo = aoo+ao;
+        aoo = aoo+a[i];
     }
     aoo = aoo*h2;
    // printf("%.4f\n", aoo);
 
 
-
-
-    w = (2*PI)/T;
     for (tt = 0.00; tt <= 0.001; tt+=0.001/N)
     {
         for (k = 1; k <= 10; k++)
         {
             for (int i = 0; i < t1; i++)
             {
-                ak = a[i]*cos(k*w*b[i]);
-                akk+=ak;
-                bk = a[i]*sin(k*w*b[i]);
-                bkk+=bk;
+                akk += a[i] * cos(k * w * b[i]);
+                bkk += a[i] * sin(k * w * b[i]);
             }
-            akk = akk*h2;
-            bkk = bkk*h2;
-            smt = (akk*cos(k*w*tt)+bkk*sin(k*w*tt));
-            smtt+=smt;
-            akk = 0.0;
-            bkk = 0.0;
+            akk = multiplex(akk, h2);
+            bkk = multiplex(bkk, h2);
+            smtt += sumOffurier(akk, k, tt, bkk);
         }
-        smtt+= aoo/2;
+        smtt += aoo / 2;
 //        printf("%.10f\t", tt);
         fprintf(ffurr,"%f\t", tt);
 //        printf("%.10f\n", smtt);
         fprintf(ffurr,"%f\n", smtt);
 
-        smtt = 0;
     }
     cout << "t " << t << endl;
     cout << "t1 " << t1 << endl;
     cout << "PI " << PI << endl;
+//    cout << "PI " << setprecision(10) << PI * 1035*1035 << endl;
+//    cout << "PI " << setprecision(10) << 3.14159 * 1035*1035 << endl;
+    cout << setprecision(100) << w << endl;
 
     return 0;
 }
